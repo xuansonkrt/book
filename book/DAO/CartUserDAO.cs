6f53dc05fdb21f04bdaa42727a8221b9111d7308
xuansonkrt <xuansonkrt@gmail.com>
2019-05-24 19:15:29 +0700

    manh
    cuar Manhj

---
+++ book/DAO/CartUserDAO.cs
@@ -0,0 +1,49 @@
+﻿using book.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace book.DAO
+{
+    public class CartUserDAO
+    {
+        private MyDBContext db;
+        public CartUserDAO()
+        {
+            db = new MyDBContext();
+        
+        }
+        public int getID(int id)// get id cart
+        {   
+            var sql = " select * from Cart where ID_Account = " + id;
+            var lst = db.Carts.SqlQuery(sql).ToList();
+            if(lst.Count == 0)
+            {
+                return -1;
+            }
+
+            return lst[0].ID;
+        }
+        public int? GetQuanity( int idbook, int idcart)
+       
+        {
+            var sql = " select * from CartDetail where ID_Book=" + idbook + " and ID_Cart=" + idcart;
+            var lst = db.CartDetails.SqlQuery(sql).ToList();
+            if( lst.Count == 0  )
+            {
+                return -1;
+            }
+            return lst[0].Quantity;
+        }
+        public int GetAmount( int id)
+        {
+            var sql = "exec Dem " + id;
+            var lst = db.Database.SqlQuery<int>(sql).FirstOrDefault();
+            return lst;
+                        
+                     
+                     
+        }
+    }
+}
